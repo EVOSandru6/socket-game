320c782ac975a3fece6faccb82244f111fd83077
EVOSandru6 <evosduple@mail.ru>
2020-04-09 02:47:32 +0300

    add server dir

---
+++ app/server/src/index.js
@@ -0,0 +1,11 @@
+console.log('server running');
+
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/hello', function(req, res) {
+    res.send('hello world');
+});
+
+app.listen(port);
